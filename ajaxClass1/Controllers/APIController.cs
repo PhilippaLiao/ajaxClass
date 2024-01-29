3d12f6370ab0dd34575bd5265d9432e3b7bc1444
philippa <philippaliao@outlook.com>
2024-01-29 16:57:32 +0800

    加入專案檔案。

---
+++ ajaxClass1/Controllers/APIController.cs
@@ -0,0 +1,24 @@
+﻿using ajaxClass1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ajaxClass1.Controllers
+{
+    public class APIController : Controller
+    {
+        private MyDBContext _context;
+        public APIController(MyDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cities()
+        {
+            var cities = _context.Addresses.Select(c => c.City).Distinct();
+            return Json(cities);
+        }
+    }
+}
